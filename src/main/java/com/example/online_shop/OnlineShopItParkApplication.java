2f6c2c407f6af3aeed57baea7c3ec4f5bb2c3b43
Azizbek <azizbekabdulaxatov2001@gmail.com>
2022-11-05 05:51:24 -0700

    Initial commit

---
+++ src/main/java/com/example/online_shop/OnlineShopItParkApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopItParkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShopItParkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/online_shop/OnlineShopItParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopItParkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
